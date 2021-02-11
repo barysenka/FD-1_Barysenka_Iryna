4f50734953c44d9604517a1a67e48439f9c9b62e
unknown <iborisenkoip@gmail.com>
2021-02-11 15:25:10 +0300

    create JS_task2

---
+++ JS_task2/JS_Task2.js
@@ -0,0 +1,92 @@
+// ***Task1
+
+// let num = +prompt();
+// let mult =1;
+// while (num){
+//    let rem = num % 10;
+//     mult *= rem;
+//     num = (num - rem)/10; 
+// }
+// console.log(mult)
+
+
+// ***Task2
+
+// let a = +prompt();
+// let b = +prompt();
+// if (a > b) {
+// a = a + b;
+// b = a - b;
+// a = a -b;
+// }
+
+// for (let i = a; i <=b; i++) {
+//     console.log(i)
+// }
+
+// ***Task3
+
+// ***Task4
+
+// let browser = prompt('');
+// if (browser === 'Edge' || browser === 'IE' || browser === 'Opera') {
+//     alert ('partial support');
+// } 
+// if (browser === 'Chrome' || browser === 'Mozila' || browser === 'Safari') {
+//     alert ('support');
+// } 
+// else {
+//     alert('not support');
+// }
+
+// let information = (browser === 'Edge' || browser === 'IE' || browser === 'Opera') ? 'partial support':
+// (browser === 'Chrome' || browser === 'Mozila' || browser === 'Safari') ? 'support':
+// 'not support';
+// alert(information);
+
+
+// ***Task5
+
+// let a = +prompt();
+// let b = +prompt();
+// let c = +prompt();
+// if (a > b) {
+// a = a + b;
+// b = a - b;
+// a = a -b;
+// }
+
+// for (let i = a; i <=b; i = i + c) {
+
+//     console.log(i)
+// }
+
+// ***Task6
+
+// let a = +prompt();
+// let i = 1;
+// let c = a;
+// while(i < a){
+// let b = (a - i);
+// c *= b; 
+// i++; 
+// }
+// console.log(c)
+
+// ***Task7
+
+// let num = +prompt();
+// let a = 1;
+// let b = 2;
+// let i = 3;
+// console.log(a)
+// console.log(b)
+// while(i <= num){
+// let c = a;
+// a = b; 
+// b = c + a; 
+// i++; 
+// console.log(b);
+// }
+
+ 
