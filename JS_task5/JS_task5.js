672e31cece4e50af5b78201df7e05c3171d10f9d
unknown <iborisenkoip@gmail.com>
2021-03-05 09:58:55 +0300

    t4 JS_task5

--- JS_task5/JS_task5.js
+++ JS_task5/JS_task5.js
@@ -1,197 +1,89 @@
 
+
+
+
 // // ***Task1
 
+// function getMaxNumder (arr){
+//     let MaxNumder = arr[0];
+//     for (let i = 0; i < arr.length; i++){
 
-// let num = [1, 3, 4, 8, 2, 9];
-// let min = num[0];
-// let max = num[0];
-// let positionMin = 0;
-// let positionMax = 0;
-// for (let i = 0; i < num.length; i++){
-//     if (min > +num[i]){
-//         min = +num[i]; 
-//         positionMin = i;    
-//     }else if (max < +num[i]){
-//         max = +num[i]; 
-//         positionMax = i;    
+//         if (MaxNumder < arr[i]){
+//             MaxNumder = arr[i];
+//         }
 //     }
+//     return MaxNumder;
 // }
-// num[positionMin] = num[positionMax] + num[positionMin];
-// num[positionMax] = num[positionMin] - num[positionMax];
-// num[positionMin] = num[positionMin] - num[positionMax];
 
-// console.log(num);
+// let arr2 = [5, 8, 2, 1, 3, 2];
+// let arr = [1, 0, 4, 9, 4, 7];
+// console.log(getMaxNumder(arr));
+
+// console.log(getMaxNumder(arr2));
 
 
-// // ***Task2 
+// // ***Task2
 
-// let isPrime = false;
-// let arr = [4, 2, 3, 5, 3, 2, 4];
-// let n = arr.length - 1;
-// for (let i = 0; i < arr.length/2; i++){
-//      if (arr[i] !== arr[n - i]){
-//         isPrime = false;
-//         break;
-//      } else if (arr[i] === arr[n - i]){
-//         isPrime = true;
+// function getReversArr (arr){
+//     let newArr = [];
+//     for (i = 0; i < arr.length; i++){
+//         newArr.push(arr[(arr.length - 1) - i]);
 //     }
+//     return newArr;
 // }
-// if (isPrime){
-//     console.log('yes');
-// } else {
-//     console.log('no');
-//     }
+
+// console.log(getReversArr(arr));
+// console.log(getReversArr(arr2));
 
 
 // // ***Task3
 
-// let arr = [8, 3, 1, 7, 4, 7, 5, 4, 3, 6];
+// function getIndex(arr, firstInd, secondInd){
+//         let a = arr[firstInd];
+//         arr[firstInd] = arr[secondInd];
+//         arr[secondInd] = a;
 
-// for (let i = 0; i < arr.length; i++){
-//     for (let j = i + 1 ; j < arr.length; j++){
-//         if (arr[i] === arr[j]){
-//         arr.splice(j, 1);
-//         }
-//     }  
-// }  
-// console.log(arr);
+//         return arr;
+//     }
+// let arr = [1, 5, 3, 6, 8, 2];
+// let arr1 = [1, 5, 3, 6, 8, 2];
+// console.log(getIndex(arr, 2, 4));
+// console.log(getIndex(arr1, 1, 5));
 
 
 
 // // ***Task4
 
-// let arr = [1, 3, 4, 8, 2, 9, 5];
-// let max = arr[1];
-// for (let i = 1; i < arr.length; i+= 2){
-//    if (max < arr[i]){
-//         max = arr[i];    
-//     }
-// }
-// console.log(max);
-
-
 
 // // ***Task5
+function getSumFigure(num) {
+    let sum = 0;
+    let numStr = String(num);
+  
+    for(let i = 0; i < numStr.length; i++) {
+      sum += Number(numStr[i]);
+    }
+  
+  return sum;
+}
 
-// let num = [2, 3, 1, 8, 2, 5, 9, 5];
-// let min = num[0];
-// let max = num[0];
-// let positionMin = 0;
-// let positionMax = 0;
-// let sum = 0;
-// for (let i = 0; i < num.length; i++){
-//     if (min > num[i]){
-//         min = num[i]; 
-//         positionMin = i;    
-//     } else if (max < +num[i]){
-//         max = num[i]; 
-//         positionMax = i;    
-//     }
-// }
-// if (positionMin > positionMax){
-//     positionMin = positionMax + positionMin;
-//     positionMax = positionMin - positionMax;
-//     positionMin = positionMin - positionMax;
-// }
-// for (let j = positionMin + 1; j < positionMax; j++){
-//     sum += +num[j];
-// }
-
-// console.log(sum);
+console.log(getSumFigure(252511));
 
 
 
 // // ***Task6
 
-// let arr = [1, 3, 1, 2, 2, 5, 2, 5, 3, 1, 5, 1];
-
-// let arr2 = [];
-
-// for (let i = 0; i < arr.length; i++){
-//     if (arr2[arr[i]]) {
-//       arr2[arr[i]]++;
-//     } else {
-//       arr2[arr[i]] = 1;
-//     }
-// }
-
-// let max = arr2[0];
-// let maxIndex = 0;
-
-
-// for(let i = 0; i < arr2.length; i++) {
-//   if (arr2[i] > max || max === undefined) {
-//       max = arr2[i];
-//       maxIndex = i;
-//   }
-// }
-// console.log(maxIndex);
-
-
-
-// // ***Task7
-
-// let arr = [2, 4, 6, 1, 3];
-// let sum = 0;
-// let newArr =[];
-
-// for (let i = 0; i < arr.length; i++){
-//     sum += +arr[i];
-// }
-
-// let arith = sum / arr.length;
-
-// for (let j = 0; j < arr.length; j++){
-//     if (arith > arr[j]){
-//         newArr.push(arr[j]);
-//     }
-// }
-// console.log(newArr);
-
-// // ***Task8
-
-// let arr = [1, 2, 3, 4, 5, 6, 7];
-// let n = arr.length - 1;
-// let newArr = [];
-// for (let i = n; i >= 0; i--){
-//    newArr.push(arr[i]);
-// }
-// console.log(newArr);
-
-
-// // ***Task9
-
-// let arr = [4, -5, 2,- 4, -2, 2, 1, 5];
-// let newArr = [];
-
-// for(let i = 0; i < arr.length; i++){
-//     if(arr[i] > 0){
-//         newArr.push(arr[i]);
-//     }
-// }
-// for(let j = 0; j < arr.length; j++){
-//     if(arr[j] < 0){
-//         newArr.push(arr[j]);
-//     }
-// }
-// console.log(newArr);
-
-
-
-// // ***Task10 
-
-// let arr = [2, 5, -9, 2, -5, 10, 2, -2];
-
-//  let maxSum = 0; 
-
-//   for (let i = 0; i < arr.length; i++) {
-//     let sumFixedStart = 0;
-//     for (let j = i; j < arr.length; j++) {
-//       sumFixedStart += arr[j];
-//       maxSum = Math.max(maxSum, sumFixedStart);
-//     }
-//   }
-// console.log(maxSum)
-
-
-
+function getGCD(a, b) {
+    const min = a < b ? a : b;
+    let gcd = 1;
+  
+    for(let i = min; i > 0; i--) {
+      if (a % i === 0 && b % i === 0) {
+        gcd = i;
+        break;
+      }
+    }
+  
+    return gcd;
+}
+console.log(getGCD(89, 12));
